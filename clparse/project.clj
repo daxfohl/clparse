7d5baa7e9b24d06ad976181c71d6fe522aec87c3
Dax Fohl <dax.fohl@gmail.com>
2011-11-04 16:21:39 -0500

    First Commit

---
+++ clparse/project.clj
@@ -0,0 +1,6 @@
+(defproject clparse "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [enlive "1.0.0"]]
+  :main clparse.core)
---
+++ clparse/test/clparse/test/core.clj
@@ -0,0 +1,6 @@
+(ns clparse.test.core
+  (:use [clparse.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
