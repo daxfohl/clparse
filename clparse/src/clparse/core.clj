7fa1a77f469cd95791f03a0bdecdb5322554dc6f
Dax Fohl <dax.fohl@gmail.com>
2011-11-07 12:26:33 -0600

    Thru works

--- clparse/src/clparse/core.clj
+++ clparse/src/clparse/core.clj
@@ -25,6 +25,14 @@
   
 (def date-formatter (tmf/formatter "MM/dd/yyyy"))
 
+(defn get-works-2 [inner]
+  (def splits (.split inner ","))
+  (def trims (map (fn [x] (.trim x)) splits))
+  (vec trims))
+
+(defn get-works [colonSplit]
+  (def ex (> (count colonSplit) 1))
+  (if ex (get-works-2 (nth colonSplit 1)) []))
 
 (defn toItem [tr]
   (def td (vec (html/select tr [:td])))
@@ -33,12 +41,12 @@
   (def split (.split desc " "))
   (def date (tmf/parse date-formatter (nth split 0)))
   (def colonSplit (.split desc ":"))
-  (def task (.substring (nth colonSplit 0) (+ 3 (.length (nth split 0)))))
-  ;(def works (
-  task)
+  (def task (subs (nth colonSplit 0) (+ 3 (count (nth split 0)))))
+  (def works (get-works colonSplit)) 
+  works)
 
 (def items
-  (map toItem trs))
+  (vec (map toItem trs)))
 
 (defn -main []
   (pp/pprint items))
