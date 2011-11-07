ae5d8b2abbf86c823219fe877629cf3bfdb1de95
Dax Fohl <dax.fohl@gmail.com>
2011-11-07 12:07:36 -0600

    Up to Works

--- clparse/project.clj
+++ clparse/project.clj
@@ -2,5 +2,6 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [enlive "1.0.0"]]
+                 [enlive "1.0.0"]
+                 [clj-time "0.3.2"]]
   :main clparse.core)
--- clparse/src/clparse/core.clj
+++ clparse/src/clparse/core.clj
@@ -1,21 +1,45 @@
 (ns clparse.core
   (:gen-class)
-  (:require [net.cgrand.enlive-html :as html]))
+  [:require 
+   [net.cgrand.enlive-html :as html]
+   [clojure.pprint :as pp]
+   [clj-time [core :as tm] [format :as tmf]]])
 
-(def *base-url* "http://news.ycombinator.com/")
+(def *base-url* "file:///C:/Users/Dax/git/clparse/clparse/invoice.html")
 
-(defn fetch-url [url]
-  (html/html-resource (java.net.URL. url)))
+(def document
+  (html/html-resource (java.net.URL. *base-url*)))
 
-(defn hn-headlines []
-  (map html/text (html/select (fetch-url *base-url*) [:td.title :a])))
+(def breakdownTable
+  (-> document
+      (html/select [:div(html/attr= :summary "Invoice Items")])
+      (first)))
 
-(defn hn-points []
-  (map html/text (html/select (fetch-url *base-url*) [:td.subtext html/first-child])))
+(def breakdownBody
+  (-> breakdownTable
+    (html/select [:tbody])
+    (first)))
 
-(defn print-headlines-and-points []
-  (doseq [line (map #(str %1 " (" %2 ")") (hn-headlines) (hn-points))]
-    (println line)))
+(def trs 
+  (html/select breakdownBody [:tr]))
+  
+(def date-formatter (tmf/formatter "MM/dd/yyyy"))
+
+
+(defn toItem [tr]
+  (def td (vec (html/select tr [:td])))
+  (def tp (html/text (nth td 0)))
+  (def desc (html/text (nth td 1)))
+  (def split (.split desc " "))
+  (def date (tmf/parse date-formatter (nth split 0)))
+  (def colonSplit (.split desc ":"))
+  (def task (.substring (nth colonSplit 0) (+ 3 (.length (nth split 0)))))
+  ;(def works (
+  task)
+
+(def items
+  (map toItem trs))
 
 (defn -main []
-  (hn-headlines))
+  (pp/pprint items))
+
