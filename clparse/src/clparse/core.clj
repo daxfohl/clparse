8402c0e50a86b655d487ae2991fb38a68bc7a898
Dax Fohl <dax.fohl@gmail.com>
2011-11-10 12:46:13 -0600

    Replace template file with string, even/odd rows, output to file.

--- clparse/src/clparse/core.clj
+++ clparse/src/clparse/core.clj
@@ -6,20 +6,55 @@
    [clojure.string :as st]
    [clj-time [core :as tm] [format :as tmf]]])
 
+(def templates (html/html-resource (new java.io.StringReader "
+<table id='summary_table' summary='Invoice Items' class='client-document-items' cellspacing='0'>
+  <thead>
+    <tr>
+      <th class='client-document-item-description'>Item</th>
+      <th class='client-document-item-amount last'>Amount</th>
+    </tr>
+  </thead>
+  <tbody class='client-document-item-rows' id='summary_body'>
+        <tr class='client-document-item-rows-odd'>
+		  <td class='client-document-item-description'><p/></td>
+		  <td class='client-document-item-amount last'/>
+        </tr>        
+        <tr/>
+		<tr>
+		  <td/>
+		  <td class='client-document-item-amount'> </td>
+		</tr>    
+		<tr class='client-document-item-rows-even'>
+		  <td class='client-document-item-description'><p>Total</p></td>
+		  <td class='client-document-item-amount last' id='summary_total'/>
+		</tr>
+  </tbody>
+</table>
+<div id='breaks'>
+	<style>.break { page-break-before: always; }</style>
+	<div class='break'/>
+</div>
+<tbody id='item_body_template' class='client-document-item-rows'>
+	<tr>
+	  <td class='client-document-item-type first'/>
+	  <td class='client-document-item-description'><p/></td>
+	  <td class='client-document-item-qty'/>
+	  <td class='client-document-item-unit-price'/>
+	  <td class='client-document-item-amount last'/>
+	</tr>
+</tbody>")))
+
 (defn currstr[x] (.format (java.text.NumberFormat/getCurrencyInstance) x))
 (defrecord WorkItem [date qty task item-type unit-price details])
 (defn get-total [work-item] (* (:unit-price work-item) (:qty work-item)))
 (defn get-total-string [work-items] (->> work-items (map get-total) (reduce +) (currstr)))
 (defn get-key [work-item] {:task (:task work-item) :item-type (:item-type work-item) :unit-price (:unit-price work-item)})
 
-(def document (html/html-resource (java.net.URL. "file:///C:/Users/Dax/git/clparse/clparse/invoice.html")))
-(def templates (html/html-resource (java.net.URL. "file:///C:/Users/Dax/git/clparse/clparse/templates.html")))
-(def client-document (html/select document [:#client_document]))
+(def document (html/html-resource (new java.io.File "invoice.html")))
 (def summary-template (html/select templates [:#summary_table]))
 (def item-body-template (html/select templates [:#item_body_template]))
 (def page-break (html/select templates [:#breaks]))
-(def breakdown-table (html/select client-document [:table.client-document-items]))
-(def breakdown-body (html/select breakdown-table [:tbody.client-document-item-rows]))
+(def breakdown-body (html/select document [:tbody.client-document-item-rows]))
 (def trs (html/select breakdown-body [:tr]))  
 (def date-formatter (tmf/formatter "MM/dd/yyyy"))
 
@@ -29,9 +64,9 @@
   (def desc (html/text (nth td 1)))
   (def split (st/split desc #" "))
   (def date (tmf/parse date-formatter (nth split 0)))
-  (def colonSplit (st/split desc #":"))
-  (def task (subs (nth colonSplit 0) (+ 3 (count (nth split 0)))))
-  (def details (if (> (count colonSplit) 1) (vec (map st/trim (st/split (nth colonSplit 1) #","))) [])) 
+  (def colon-split (st/split desc #":"))
+  (def task (subs (nth colon-split 0) (+ 3 (count (nth split 0)))))
+  (def details (if (> (count colon-split) 1) (vec (map st/trim (st/split (nth colon-split 1) #","))) [])) 
   (def qty (bigdec (html/text (nth td 2))))
   (def unit-price (bigdec (st/replace (html/text (nth td 3)) #"[^0-9\.]" "")))
   (WorkItem. date qty task item-type unit-price details))
@@ -57,7 +92,9 @@
                            [:p] (html/content (->> (val tg) (map :details) (reduce concat) (distinct) (st/join "; ")))
                            [:td.client-document-item-qty] (html/content (->> (val tg) (map :qty) (reduce +) (str)))
                            [:td.client-document-item-unit-price] (html/content (->> (key tg) (:unit-price) (currstr)))
-                           [:td.client-document-item-amount] (html/content (get-total-string (val tg))))))
+                           [:td.client-document-item-amount] (html/content (get-total-string (val tg))))           
+           [[:tr html/even]] (html/add-class "client-document-item-rows-even")           
+           [[:tr html/odd]] (html/add-class "client-document-item-rows-odd")))
 
 
 (def doc-with-tranform
@@ -65,9 +102,13 @@
            [:tbody.client-document-item-rows]
            (html/substitute item-body)
            [:table.client-document-items] 
-           (html/before [summary-table page-break])))
+           (html/before [summary-table page-break])
+           [:div.invoice-summary] 
+           (html/html-content (str "<span>Amount Due</span>" (get-total-string items)))
+           [:tr.client-document-total] 
+           (html/html-content (str "<td colspan=4>Amount Due</td><td class='total'>" (get-total-string items) "</td>"))))
 
 
 (defn -main []  
-  (println (apply str (html/emit* doc-with-tranform))))
-
+  (println (apply str (html/emit* doc-with-tranform)))
+  (spit "invoice2.html" (apply str (html/emit* doc-with-tranform))))
